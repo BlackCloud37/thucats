3ec580673c7213098607ee0f227cf47200502d9d
pissft <yechenz37@163.com>
2021-10-15 11:36:31 +0800

    Initial commit

---
+++ src/app.config.ts
@@ -0,0 +1,11 @@
+import { AppConfig } from "remax/wechat";
+
+const config: AppConfig = {
+  pages: ['pages/index/index'],
+  window: {
+    navigationBarTitleText: 'Remax Wechat Template With TypeScript',
+    navigationBarBackgroundColor: '#282c34'
+  }
+};
+
+export default config;
