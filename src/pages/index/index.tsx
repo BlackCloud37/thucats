6517a8e9b4c5449527dc8d356d3ab6c20f422974
yechen <yechenz37@163.com>
2021-10-20 00:56:01 +0800

    feat: lin-ui usage example

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -2,6 +2,7 @@ import { Dispatch, RootState } from '@/models/store';
 import * as React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { View, Image } from 'remax/wechat';
+import LButton from 'lin-ui/dist/button';
 import styles from './index.css';
 
 const Index = () => {
@@ -10,9 +11,7 @@ const Index = () => {
     loading: state.loading.effects.users.fetchOpenidAsync // true when `fetchOpenidAsync` is running
   }));
   const dispatch = useDispatch<Dispatch>();
-  React.useEffect(() => {
-    dispatch.users.fetchOpenidAsync();
-  }, []);
+  const { fetchOpenidAsync } = dispatch.users;
 
   return (
     <View className={styles.app}>
@@ -21,12 +20,19 @@ const Index = () => {
           src="https://gw.alipayobjects.com/mdn/rms_b5fcc5/afts/img/A*OGyZSI087zkAAAAAAAAAAABkARQnAQ"
           className={styles.logo}
         />
-        {loading ? (
-          <View className={styles.text}>Loading</View>
-        ) : (
-          <View className={styles.text}>OpenID: {openid}</View>
-        )}
       </View>
+      <LButton
+        l-class={styles.button}
+        bg-color="black"
+        bindlintap={() => {
+          fetchOpenidAsync().catch((err) => console.error(err));
+        }}
+        loading={loading}
+        disabled={!!openid}
+      >
+        {openid ? 'Banned' : loading ? 'Loading' : 'Click me'}
+      </LButton>
+      {openid && <View className={styles.text}>OpenID: {openid}</View>}
     </View>
   );
 };
---
+++ typings/lin-ui/dist/button.d.ts
@@ -0,0 +1,7 @@
+import { ComponentType } from 'react';
+declare const LButton: ComponentType<{
+  bindlintap?: any;   // 按钮在非禁用状态下点击所触发的事件
+  loading?: boolean;  // 是否为加载中按钮
+  disabled?: boolean; // 按钮是否禁用
+}>;
+export default LButton;
