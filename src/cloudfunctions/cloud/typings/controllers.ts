a2478361523bcd4f412a9b691880ca0965b38d74
pissft <yechenz37@163.com>
2021-10-21 20:27:41 +0800

    fix(rollup-err): ts-enum export must be `const`

---
+++ src/cloudfunctions/cloud/typings/controllers.ts
@@ -0,0 +1,21 @@
+export const enum EController {
+  // Modify: add new controller
+  User = 'user',
+  Cat = 'cat'
+}
+
+// prettier-ignore
+// Modify: add Controller to Action
+export type ActionFor<C extends EController> = 
+  C extends EController.User ? EUserActions :
+  C extends EController.Cat  ? ECatAcions:
+  never;
+
+// Modify: add new EActions
+export const enum EUserActions {
+  GetOpenid = 'getOpenid'
+}
+
+export const enum ECatAcions {
+  SomeMethod = 'somemethod'
+}
