a3a6f9a5d4c2826d1f352c8ed6c60495ba5577e6
yechen.z <yechen.z@bytedance.com>
2021-10-28 11:10:06 +0800

    fix: adapt wx-storage to persist storage

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -3,19 +3,20 @@ import { getStorage, setStorage, removeStorage } from 'remax/wechat';
 const createStorage = () => {
   return {
     getItem: (key: string) => {
-      console.log('getItem');
       return new Promise((resolve, _reject) => {
-        resolve(getStorage({ key }));
+        getStorage({ key })
+          .then(({ data }) => {
+            resolve(data);
+          })
+          .catch(console.error);
       });
     },
     setItem: (key: string, item: any) => {
-      console.log('setItem', JSON.stringify(item, null, '\t'));
       return new Promise((resolve, _reject) => {
         resolve(setStorage({ key, data: item }));
       });
     },
     removeItem: (key: string) => {
-      console.log('removeItem');
       return new Promise((resolve, _reject) => {
         resolve(removeStorage({ key }));
       });
