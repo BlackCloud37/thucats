8d20912ca53e84ab0183eb2385b962b1c05873f4
EdwardFrog <changkehua@gmail.com>
2021-10-29 00:42:17 +0800

    feat: tabbar

---
+++ src/typings/lin-ui/dist/tab-bar.d.ts
@@ -0,0 +1,10 @@
+import { ComponentType } from 'react';
+declare const LTabBar: ComponentType<{
+  list: {
+    pagePath: string,
+    text: string,
+    iconPath: string,
+    selectedIconPath: string
+  }[];
+}>;
+export default LTabBar;
