710b7c984e4a7defa470e65d2a7661275d6bd729
yechen.z <yechen.z@bytedance.com>
2021-10-29 13:22:04 +0800

    feat: add tailwind css support

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,10 +1,11 @@
 import { Dispatch, RootState } from '@/models/store';
 import * as React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { View } from 'remax/wechat';
+import { View, Text, Image } from 'remax/wechat';
 import LButton from 'lin-ui/dist/button';
 import styles from './index.css';
 import { navigateTo } from '@/utils';
+import '@/styles/tailwind.css';
 
 const Index = () => {
   const { openid, loading } = useSelector((state: RootState) => ({
@@ -16,6 +17,16 @@ const Index = () => {
 
   return (
     <View className={styles.app}>
+      <View className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4 mt-2">
+        <View className="flex-shrink-0">
+          <Image className="h-12 w-12 mx-2" src="/icon.png" />
+        </View>
+        <View>
+          <View className="text-xl font-medium text-black text-left">Powered by Remax</View>
+          <Text className="text-gray-500">Tailwind test block</Text>
+        </View>
+      </View>
+
       <View className={styles['nav-card-container']}>
         <View onClick={() => navigateTo('cat-list')}>图鉴</View>
         <View onClick={() => navigateTo('404')}>x</View>
