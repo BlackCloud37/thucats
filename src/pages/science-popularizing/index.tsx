8bad4a92eb83041e335199ec666af652991b054e
yechen <yechenz37@163.com>
2021-11-02 16:37:40 +0800

    feat: abstract tabs

--- src/pages/science-popularizing/index.tsx
+++ src/pages/science-popularizing/index.tsx
@@ -1,45 +1,9 @@
 import * as React from 'react';
 import { View } from '@remax/wechat';
-import classNames from 'classnames';
 import Photo from '@/components/photo';
+import { Tabs, TabPanel } from '@/components/tabs';
 import { navigateTo } from '@/utils';
 import { usePageEvent } from '@remax/macro';
-const Tabs = (props: { children: React.ReactElement[] }) => {
-  const { children } = props;
-  const tabs = React.Children.map(children, (el) => el.props.tab);
-  const [activeTab, setTab] = React.useState(tabs[0]);
-  const tabCount = tabs.length;
-  const activeTabIdx = tabs.findIndex((t) => t === activeTab);
-  const activeTabEl = children[activeTabIdx];
-  return (
-    <View className="flex flex-col rounded-lg shadow-xl h-full bg-white">
-      <View className="flex flex-row flex-nowrap h-10 w-full flex-shrink-0">
-        {tabs.map((tab, index) => (
-          <View
-            key={tab}
-            onClick={() => setTab(tab)}
-            className={classNames('font-light text-sm flex-grow flex items-center justify-evenly', {
-              'bg-gray-200': activeTab !== tab,
-              'rounded-lg': activeTab === tab,
-              'rounded-tr-lg': index === tabCount - 1,
-              'rounded-tl-lg': index === 0,
-              'rounded-bl-lg': index === activeTabIdx + 1,
-              'rounded-br-lg': index === activeTabIdx - 1
-            })}
-          >
-            {tab}
-          </View>
-        ))}
-      </View>
-      <View className="rounded-lg p-1">{activeTabEl}</View>
-    </View>
-  );
-};
-
-const TabPanel = (props: { children: any; tab: string }) => {
-  const { children } = props;
-  return <View>{children}</View>;
-};
 
 const SciencePopularizingPage = () => {
   usePageEvent('onShareAppMessage', () => ({
