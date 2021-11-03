587361d816ad8f207f6197f4354913e77956c14b
yechen <yechenz37@163.com>
2021-11-03 23:23:49 +0800

    feat: abstract configs

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -7,7 +7,7 @@ const About = () => {
     <>
       <View className="m-5 p-5 bg-white rounded-lg shadow-xl flex flex-col items-start font-light text-sm">
         <Image
-          src="/images/full-logo-transparent.png"
+          src="/personal/full-logo-transparent.png"
           mode="widthFix"
           className="w-2on3 mb-5 self-center"
         />
@@ -22,7 +22,6 @@ const About = () => {
         <Text
           className="block mb-2"
           onClick={() => {
-            console.log('clicke');
             setClipboardData({
               data: 'yechenz37@163.com',
               success: () =>
@@ -42,10 +41,20 @@ const About = () => {
           appId="wxebadf544ddae62cb"
           path="pages/survey/index?sid=9239520&hash=6765"
           version="release"
+          className="mb-5"
         >
           <Text className="text-center underline text-blue-500">反馈问卷</Text>
         </Navigator>
+
+        <Text className="block text-gray-500">致谢</Text>
+        <Text className="block mb-1">
+          本项目受到大量已有的、开源的猫咪图鉴项目的启发，并多有参考，对这些项目表示万分感谢:
+        </Text>
+        <Text className="block mb-1">- 燕园猫猫速查手册</Text>
+        <Text className="block mb-1">- 中大猫谱</Text>
+        <Text className="block mb-5">- 科大猫咪相簿</Text>
       </View>
+
       <TabBar />
     </>
   );
