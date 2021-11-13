6f0762c899f0bed75f60300ee4b81c7c5db9f968
yechen <yechenz37@163.com>
2021-11-13 15:30:30 +0800

    feat: add clipable component

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,9 +1,10 @@
 import TabBar from '@/components/tabbar';
 import { RootState } from '@/models/store';
-import { Text, View, Image, setClipboardData, showToast } from '@remax/wechat';
+import { Text, View, Image } from '@remax/wechat';
 import * as React from 'react';
 import { useSelector } from 'react-redux';
 import Wemark from '@/components/wemark/wemark';
+import Clipable from '@/components/clipable';
 
 const About = () => {
   const { associationLogo, associationIntroduction } = useSelector((state: RootState) => ({
@@ -16,21 +17,11 @@ const About = () => {
       <View className="m-5 p-5 bg-white rounded-lg shadow-xl flex flex-col items-start font-light text-sm">
         <Image webp src={associationLogo} mode="widthFix" className="w-2on3 mb-5 self-center" />
         <Wemark md={associationIntroduction} />
-        <Text
-          className="block mb-5 w-full text-center"
-          onClick={() => {
-            setClipboardData({
-              data: 'https://github.com/BlackCloud37/thucats/wiki/部署流程',
-              success: () => {
-                showToast({
-                  title: '网址已复制'
-                });
-              }
-            });
-          }}
-        >
-          一键部署教程已经发布在<Text className="underline text-blue-500">Github</Text>，欢迎体验
-        </Text>
+        <Clipable clipContent="https://github.com/BlackCloud37/thucats/wiki/部署流程">
+          <Text className="block mb-5 w-full text-center">
+            一键部署教程已经发布在<Text className="underline text-blue-500">Github</Text>，欢迎体验
+          </Text>
+        </Clipable>
       </View>
       <TabBar />
     </>
