0dd6d17ac21d370be4922f394bc74ad11ea1cace
yechen <yechenz37@163.com>
2021-12-05 13:39:35 +0800

    build: shake unused wx interfaces such as getWeRunData
    close #59

--- remax.config.js
+++ remax.config.js
@@ -1,1 +1,37 @@
-module.exports = {};
+module.exports = {
+  configWebpack({ config }) {
+    config.module
+      .rule('shake')
+      .test(/index.js$/)
+      .use('string-replace-loader')
+      .loader('string-replace-loader')
+      .options({
+        multiple: [
+          'getWeRunData',
+          'chooseInvoiceTitle',
+          'chooseInvoice',
+          'chooseLicensePlate',
+          'openBluetoothAdapter',
+          'createBLEPeripheralServer',
+          'addPhoneContact',
+          'addPhoneRepeatCalendar',
+          'addPhoneCalender'
+        ]
+          .map((search, index) => {
+            const replace = `REPLACEMENT${index}`;
+            return [
+              {
+                search,
+                replace
+              },
+              {
+                search: `wx.${search}`,
+                replace
+              }
+            ];
+          })
+          .flat()
+      })
+      .end();
+  }
+};
